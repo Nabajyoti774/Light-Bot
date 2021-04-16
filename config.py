ef803775b9068dad0e49bc5ba0a088b43af578d9
Nabajyoti774 <82664346+Nabajyoti774@users.noreply.github.com>
2021-04-16 20:47:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 
 def get_user_list(config, key):
     with open("{}/SaitamaRobot/{}".format(os.getcwd(), config), "r") as json_file:
-        return json.load(json_file)[key]
+        return json.load(json_file)[1754475184:AAEnaNpo9L9y1gf6LGXypgQgo-_EhYSU71Q]
 
 
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
@@ -16,7 +16,7 @@ class Config(object):
 
     API_ID = 12344  # integer value, dont use ""
     API_HASH = "awoo"
-    TOKEN = 1754475184:AAEnaNpo9L9y1gf6LGXypgQgo-_EhYSU71Q  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1546356049  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "God of Death"
     )  # Prints any new group the bot is added to, prints just the name and ID.
